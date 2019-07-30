fa586d3d6f91e8384d6058bda970eff644bbb4ed
Mrsfrity <1612783144@qq.com>
2019-07-30 23:35:37 +0800

    封装方法

--- src/com/ll/Multiply99.java
+++ src/com/ll/Multiply99.java
@@ -4,7 +4,11 @@ public class Multiply99 {
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 调用方法
+		multiply99_1();
+	}
+	
+	public static void multiply99_1() {
 		int a,b,c;
 		for(a = 1;a<10;a++) {
 			for(b = 1;b<=a;b++) {
@@ -14,6 +18,15 @@ public class Multiply99 {
 					System.out.println();
 				}
 			}
+		}
+	}
+	
+	public static void multiply99_2() {
+		for(int i=1; i <= 9; i++ ) {
+			for(int j=1; j <= i; j++) {
+				System.out.print(i + " * " + j + " = " + i*j);
+			 }
+			System.out.println();
 		}
 	}
 
