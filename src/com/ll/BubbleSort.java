3413439a275122a391004b5e2d8b039574f0738f
Mrsfrity <1612783144@qq.com>
2019-08-01 22:16:38 +0800

    格式化

--- src/com/ll/BubbleSort.java
+++ src/com/ll/BubbleSort.java
@@ -4,11 +4,11 @@ public class BubbleSort {
 
 	public static void bubblesort(int arr[]) {
 		//外层循环,循环5次
-		for(int i=0;i<arr.length-1;i++) {
+		for(int i = 0; i < arr.length-1; i++) {
 			//相邻对比，外层循环一次，则对比次数减一
-			for(int j =0;j<arr.length-i-1;j++) {
+			for(int j = 0; j < arr.length-i-1; j++) {
 				//相邻对比，升序排列
-				if(arr[j]>arr[j+1]) {
+				if(arr[j] > arr[j+1]) {
 					int c = arr[j];
 					arr[j] = arr[j+1];
 					arr[j+1] = c;
@@ -19,13 +19,13 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		int[] arr = {4,3,6,1,7,5};
 		System.out.println("原数组：");
-		for(int a = 0;a<arr.length;a++) {
+		for(int a = 0;a < arr.length; a++) {
 			System.out.print(arr[a]);
 		}
 		System.out.println();
 		bubblesort(arr);
 		System.out.println("排序后为：");
-		for(int b = 0;b<arr.length;b++) {
+		for(int b = 0; b < arr.length; b++) {
 			System.out.print(arr[b]);
 		}
 	}
